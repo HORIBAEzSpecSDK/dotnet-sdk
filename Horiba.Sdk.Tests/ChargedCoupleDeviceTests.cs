7e7837b6c710e033b1dea43ab1434496d92f72d0
Pavel <pavel.pavlov@zuhlke.com>
2024-04-08 14:07:59 +0300

    add tests for majority of CCD commands

--- Horiba.Sdk.Tests/ChargedCoupleDeviceTests.cs
+++ Horiba.Sdk.Tests/ChargedCoupleDeviceTests.cs
@@ -102,4 +102,160 @@ public class ChargedCoupleDeviceTests
     }
     
     // TODO test procedure get picture, move mono, get picture again
+
+    [Fact]
+    public async Task GivenCcd_WhenOpeningConnection_ThenConnectionIsOpened()
+    {
+        // Arrange
+        var dm = new DeviceManager();
+        await dm.StartAsync();
+        var ccd = dm.ChargedCoupledDevices.First();
+        
+        // Act
+        await ccd.OpenConnectionAsync();
+        var actualStatus = await ccd.IsConnectionOpenedAsync();
+
+        // Assert
+        actualStatus.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task GivenCcd_WhenOpeningAndClosingConnection_ThenConnectionIsClosed()
+    {
+        // Arrange
+        var dm = new DeviceManager();
+        await dm.StartAsync();
+        var ccd = dm.ChargedCoupledDevices.First();
+        
+        // Act
+        await ccd.OpenConnectionAsync();
+        await ccd.CloseConnectionAsync();
+        var actualStatus = await ccd.IsConnectionOpenedAsync();
+
+        // Assert
+        actualStatus.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task GivenCcd_WhenGettingSpeed_ThenReturnsSpeed()
+    {
+        // Arrange
+        var dm = new DeviceManager();
+        await dm.StartAsync();
+        var ccd = dm.ChargedCoupledDevices.First();
+        await ccd.OpenConnectionAsync();
+        
+        // Act
+        var speed = await ccd.GetSpeedAsync();
+
+        // Assert
+        speed.MatchSnapshot();
+    }
+
+    [Fact]
+    public async Task GivenCcd_WhenSettingExposureTime_ThenSetsExposureTimeCorrectly()
+    {
+        // Arrange
+        var dm = new DeviceManager();
+        await dm.StartAsync();
+        var ccd = dm.ChargedCoupledDevices.First();
+        await ccd.OpenConnectionAsync();
+        var time = 1234;
+        
+        // Act
+        await ccd.SetExposureTimeAsync(time);
+        var actualTime = await ccd.GetExposureTimeAsync();
+
+        // Assert
+        actualTime.Should().Be(time);
+    }
+
+    [Fact]
+    public async Task GivenCcd_WhenSettingXAxisConversionType_ThenSetsXAxisConversionType()
+    {
+        // Arrange
+        var dm = new DeviceManager();
+        await dm.StartAsync();
+        var ccd = dm.ChargedCoupledDevices.First();
+        await ccd.OpenConnectionAsync();
+        var targetConversionType = ConversionType.FromCcdFirmware;
+
+        // Act
+        await ccd.SetXAxisConversionTypeAsync(targetConversionType);
+        var actual = await ccd.GetXAxisConversionTypeAsync();
+        
+        // Assert
+        actual.Should().Be(targetConversionType);
+    }
+
+    [Fact]
+    public async Task GivenCcd_WhenSettingNumberOfAverages_ThenSetsTheNumberOfAverages()
+    {
+        // Arrange
+        var dm = new DeviceManager();
+        await dm.StartAsync();
+        var ccd = dm.ChargedCoupledDevices.First();
+        await ccd.OpenConnectionAsync();
+        var targetNumber = 5;
+
+        // Act
+        await ccd.SetNumberOfAveragesAsync(targetNumber);
+        var actual = await ccd.GetNumberOfAveragesAsync();
+
+        // Assert
+        actual.Should().Be(targetNumber);
+    }
+
+    [Fact]
+    public async Task GivenCcd_WhenSettingTimeResolution_ThenSetsTheTimeResolution()
+    {
+        // Arrange
+        var dm = new DeviceManager();
+        await dm.StartAsync();
+        var ccd = dm.ChargedCoupledDevices.First();
+        await ccd.OpenConnectionAsync();
+        var targetResolution = 5;
+
+        // Act
+        await ccd.SetTimerResolutionAsync(targetResolution);
+        var actual = await ccd.GetTimerResolutionAsync();
+
+        // Assert
+        actual.Should().Be(targetResolution);
+    }
+
+    [Fact]
+    public async Task GivenCcd_WhenSettingAcquisitionCount_ThenSetsTheAcquisitionCount()
+    {
+        // Arrange
+        var dm = new DeviceManager();
+        await dm.StartAsync();
+        var ccd = dm.ChargedCoupledDevices.First();
+        await ccd.OpenConnectionAsync();
+        var targetCount = 5;
+
+        // Act
+        await ccd.SetAcquisitionCountAsync(targetCount);
+        var actual = await ccd.GetAcquisitionCountAsync();
+
+        // Assert
+        actual.Should().Be(targetCount);
+    }
+
+    [Fact]
+    public async Task GivenCcd_WhenSettingCleanCount_ThenSetsCleanCount()
+    {
+        // Arrange
+        var dm = new DeviceManager();
+        await dm.StartAsync();
+        var ccd = dm.ChargedCoupledDevices.First();
+        await ccd.OpenConnectionAsync();
+
+        // Act
+        await ccd.SetCleanCountAsync(5, CleanCountMode.Mode1);
+        var actual = await ccd.GetCleanCountAsync();
+
+        // Assert
+        actual.MatchSnapshot();
+    }
 }
