60dcd0bacaeaef2b12a2d9aa8b83e5d099df61d9
ads\arbh <bharath.arun@zuehlke.com>
2024-11-22 11:56:14 +0100

    added ParallelSpeed.cs

---
+++ Horiba.Sdk/Enums/ParallelSpeed.cs
@@ -0,0 +1,10 @@
+﻿namespace Horiba.Sdk.Enums;
+
+/// <summary> 
+/// Predefined working parallel speeds
+/// </summary>
+public abstract record ParallelSpeed(int Value)
+    {
+        public int Value { get; } = Value;
+        public static explicit operator int(ParallelSpeed parallelSpeed) => parallelSpeed.Value;
+    }
