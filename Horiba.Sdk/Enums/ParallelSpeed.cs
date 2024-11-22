0166be1f59935f7520e00b42a6d70c813b2dc9da
ads\arbh <bharath.arun@zuehlke.com>
2024-11-22 13:58:20 +0100

    changed ParallelSpeed from abstract to normal class

--- Horiba.Sdk/Enums/ParallelSpeed.cs
+++ Horiba.Sdk/Enums/ParallelSpeed.cs
@@ -3,7 +3,7 @@
 /// <summary> 
 /// Predefined working parallel speeds
 /// </summary>
-public abstract record ParallelSpeed(int Value)
+public record ParallelSpeed(int Value)
     {
         public int Value { get; } = Value;
         public static explicit operator int(ParallelSpeed parallelSpeed) => parallelSpeed.Value;
