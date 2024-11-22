1c915cb4dcb157eb75c2c29ed23f51458e9c4677
ads\arbh <bharath.arun@zuehlke.com>
2024-11-22 13:58:48 +0100

    added GetParallelSpeedAsync and SetParallelSpeedAsync methods

--- Horiba.Sdk/Devices/ChargedCoupledDevice.cs
+++ Horiba.Sdk/Devices/ChargedCoupledDevice.cs
@@ -136,6 +136,30 @@ public sealed record ChargedCoupledDevice(
     }
 
     /// <summary>
+    /// Retrieves the current parallel speed of the CCD by sending the ccd_getParallelSpeed command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns><see cref="ParallelSpeed"/></returns>
+    public async Task<ParallelSpeed> GetParallelSpeedAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await Communicator.SendWithResponseAsync(new CcdGetParallelSpeedCommand(DeviceId), cancellationToken);
+        //return $"Current parallel speed token: {result.Results["token"]}";
+        return new ParallelSpeed((int)result.Results["token"]);
+    }
+
+    /// <summary>
+    /// Sets the parallel speed of the CCD by sending the ccd_setParallelSpeed command
+    /// </summary>
+    /// <param name="parallelSpeed">The <see cref="ParallelSpeed"/> to be set</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Task representing the communication between SDK and ICL</returns>
+    public Task SetParallelSpeedAsync(ParallelSpeed parallelSpeed, CancellationToken cancellationToken = default)
+    {
+        return Communicator.SendAsync(new CcdSetParallelSpeedCommand(DeviceId, parallelSpeed), cancellationToken);
+    }
+
+
+    /// <summary>
     /// Retrieves the exposure time of the CCD by sending the ccd_getExposureTime command
     /// </summary>
     /// <param name="cancellationToken"></param>
