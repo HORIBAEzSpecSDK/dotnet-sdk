7f8490d730bf22984c57da5e7f2238c3859da5e5
ads\arbh <bharath.arun@zuehlke.com>
2025-03-03 15:27:25 +0100

    added SpectrAcqDevice class

---
+++ Horiba.Sdk/Devices/SpectrAcqDevice.cs
@@ -0,0 +1,67 @@
+﻿using Horiba.Sdk.Commands;
+using Horiba.Sdk.Communication;
+using Serilog;
+
+namespace Horiba.Sdk.Devices;
+
+public sealed record SpectrAcqDevice(
+    int DeviceId,
+    string DeviceType,
+    string SerialNumber,
+    WebSocketCommunicator Communicator) :
+    Device(DeviceId, DeviceType, SerialNumber, Communicator)
+{
+    /// <summary>
+    /// Actively checks if the connection to the ICL is opened by sending a ccd_isOpen command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public override async Task<bool> IsConnectionOpenedAsync(CancellationToken cancellationToken = default)
+    {
+        var result =
+            await Communicator.SendWithResponseAsync(new SaqIsOpenCommand(DeviceId), cancellationToken);
+
+        if (result.Results.TryGetValue("open", out var bR))
+        {
+            return bool.Parse(bR.ToString());
+        }
+
+        return false;
+    }
+
+    public override Task OpenConnectionAsync(CancellationToken cancellationToken = default)
+    {
+        return Communicator.SendAsync(new SaqOpenCommand(DeviceId), cancellationToken);
+    }
+
+    public override Task CloseConnectionAsync(CancellationToken cancellationToken = default)
+    {
+        return Communicator.SendAsync(new SaqCloseCommand(DeviceId), cancellationToken);
+    }
+
+    public async Task<bool> GetAcquisitionBusyAsync(CancellationToken cancellationToken = default)
+    {
+        var result =
+            await Communicator.SendWithResponseAsync(new SaqIsBusyCommand(DeviceId), cancellationToken);
+        return (bool)result.Results["isBusy"];
+    }
+    
+    public override async Task WaitForDeviceNotBusy(int initialWaitInMs, int waitIntervalInMs,
+        CancellationToken cancellationToken = default)
+    {
+        Task.Delay(initialWaitInMs, cancellationToken).Wait(cancellationToken);
+        while (await GetAcquisitionBusyAsync(cancellationToken))
+        {
+            Log.Information("CCD: Waiting for device operation to complete");
+            Task.Delay(waitIntervalInMs, cancellationToken).Wait(cancellationToken);
+        }
+    }
+    
+    public Task WaitForDeviceNotBusy(TimeSpan? initialWait = null, TimeSpan? waitInterval = null,
+        CancellationToken cancellationToken = default)
+    {
+        var init = initialWait ?? TimeSpan.FromMilliseconds(250);
+        var wait = initialWait ?? TimeSpan.FromMilliseconds(250);
+        return WaitForDeviceNotBusy(init.Milliseconds, wait.Milliseconds, cancellationToken);
+    }
+}
