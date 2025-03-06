e68ab6968e8527c22f6a32140681ef8ed2b1328a
ads\arbh <bharath.arun@zuehlke.com>
2025-03-06 10:26:08 +0100

    added tests for checking if connection is closed and if serial number is returned correctly

--- Horiba.Sdk.Tests/SpectrAcqTests.cs
+++ Horiba.Sdk.Tests/SpectrAcqTests.cs
@@ -20,4 +20,33 @@ public class SpectrAcqTests : IClassFixture<SpectrAcqDeviceTestFixture>
         // Assert
         actual.Should().BeTrue();
     }
+    
+    [Fact]
+    public async Task GivenSaqDevice_WhenOpeningAndClosingConnection_ThenConnectionIsClosed()
+    {
+        // Act
+        await _fixture.Saq.WaitForDeviceNotBusy();
+        await _fixture.Saq.CloseConnectionAsync();
+        await _fixture.Saq.WaitForDeviceNotBusy();
+        var actual = await _fixture.Saq.IsConnectionOpenedAsync();
+
+        // Assert
+        actual.Should().BeFalse();
+    }
+    
+    [Fact]
+    public async Task GivenSaqDevice_WhenGettingSerialNumber_ThenCorrectSerialNumberIsReturned()
+    {
+        //Arrange
+        var expectedSerialNumber = "SNPG18010036";
+        
+        // Act
+        await _fixture.Saq.WaitForDeviceNotBusy();
+        var serialNumber = await _fixture.Saq.GetSerialNumberAsync();
+
+        // Assert
+        serialNumber.Should().Be(expectedSerialNumber);
+    }
+
+
 }
