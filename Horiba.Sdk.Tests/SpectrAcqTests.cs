d08dd3da3bd9070e61a32bea2e883a51b5969ccb
ads\arbh <bharath.arun@zuehlke.com>
2025-03-06 10:07:22 +0100

    added one test for SpectrAcq

---
+++ Horiba.Sdk.Tests/SpectrAcqTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Horiba.Sdk.Tests;
+
+public class SpectrAcqTests
+{
+    private readonly SpectrAcqDeviceTestFixture _fixture;
+
+    public SpectrAcqTests(SpectrAcqDeviceTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
+    [Fact]
+    public async Task GivenMonoDevice_WhenOpeningConnection_ThenConnectionIsOpened()
+    {
+        // Act
+        var actual = await _fixture.Saq.IsConnectionOpenedAsync();
+
+        // Assert
+        actual.Should().BeTrue();
+    }
+}
