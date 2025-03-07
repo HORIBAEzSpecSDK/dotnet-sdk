9ca9ca0f5e427427187e5126b01fc812d2a462d6
ads\arbh <bharath.arun@zuehlke.com>
2025-03-07 10:55:21 +0100

    added documentation for SpectrAcqDevice.cs

--- Horiba.Sdk/Devices/SpectrAcqDevice.cs
+++ Horiba.Sdk/Devices/SpectrAcqDevice.cs
@@ -15,7 +15,7 @@ public sealed record SpectrAcqDevice(
     Device(DeviceId, DeviceType, SerialNumber, Communicator)
 {
     /// <summary>
-    /// Actively checks if the connection to the ICL is opened by sending a ccd_isOpen command
+    /// Actively checks if the connection to the ICL is opened by sending a saq3_isOpen command
     /// </summary>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
@@ -32,16 +32,31 @@ public sealed record SpectrAcqDevice(
         return false;
     }
 
+    /// <summary>
+    /// Establishes a connection to the ICL by sending the saq3_open command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public override Task OpenConnectionAsync(CancellationToken cancellationToken = default)
     {
         return Communicator.SendAsync(new SaqOpenCommand(DeviceId), cancellationToken);
     }
 
+    /// <summary>
+    /// Terminates the connection to the ICL by sending the saq3_close command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public override Task CloseConnectionAsync(CancellationToken cancellationToken = default)
     {
         return Communicator.SendAsync(new SaqCloseCommand(DeviceId), cancellationToken);
     }
 
+    /// <summary>
+    /// Retrieves the acquisition busy status of the SAQ by sending the saq3_isBusy command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<bool> GetIsBusyAsync(CancellationToken cancellationToken = default)
     {
         var result =
@@ -49,6 +64,14 @@ public sealed record SpectrAcqDevice(
         return (bool)result.Results["isBusy"];
     }
 
+    /// <summary>
+    /// Starts a polling cycle to check if the device is busy. The cycle will last until the device reports
+    /// it is not busy anymore. The polling cycle will start after the initial wait time. Every polling cycle
+    /// actively sends the saq3_isBusy command
+    /// </summary>
+    /// <param name="initialWaitInMs">Defines the time before the polling cycle begins</param>
+    /// <param name="waitIntervalInMs">Defines how long will a polling cycle is</param>
+    /// <param name="cancellationToken"></param>
     public override async Task WaitForDeviceNotBusy(int initialWaitInMs = 250, int waitIntervalInMs = 250,
         CancellationToken cancellationToken = default)
     {
@@ -60,6 +83,15 @@ public sealed record SpectrAcqDevice(
         }
     }
 
+    /// <summary>
+    /// Starts a polling cycle to check if the device is busy. The cycle will last until the device reports
+    /// it is not busy anymore. The polling cycle will start after the initial wait time. Every polling cycle
+    /// actively sends the saq3_isBusy command
+    /// </summary>
+    /// <param name="initialWait"></param>
+    /// <param name="waitInterval"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task WaitForDeviceNotBusy(TimeSpan? initialWait = null, TimeSpan? waitInterval = null,
         CancellationToken cancellationToken = default)
     {
@@ -68,6 +100,11 @@ public sealed record SpectrAcqDevice(
         return WaitForDeviceNotBusy(init.Milliseconds, wait.Milliseconds, cancellationToken);
     }
 
+    /// <summary>
+    /// Retrieves the firmware version of the SAQ by sending the saq3_getFirmwareVersion command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<string> GetFirmwareVersionAsync(CancellationToken cancellationToken = default)
     {
         var result =
@@ -75,6 +112,11 @@ public sealed record SpectrAcqDevice(
         return (string)result.Results["firmwareVersion"];
     }
 
+    /// <summary>
+    /// Retrieves the FPGA version of the SAQ by sending the saq3_getFPGAVersion command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<string> GetFpgaVersionAsync(CancellationToken cancellationToken = default)
     {
         var result =
@@ -82,6 +124,11 @@ public sealed record SpectrAcqDevice(
         return (string)result.Results["FpgaVersion"];
     }
 
+    /// <summary>
+    /// Retrieves the board revision of the SAQ by sending the saq3_getBoardRevision command 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<string> GetBordRevisionAsync(CancellationToken cancellationToken = default)
     {
         var result =
@@ -89,6 +136,11 @@ public sealed record SpectrAcqDevice(
         return (string)result.Results["boardRevision"];
     }
 
+    /// <summary>
+    /// Retrieves the serial number of the SAQ by sending the saq3_getSerialNumber command 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<string> GetSerialNumberAsync(CancellationToken cancellationToken = default)
     {
         var result =
@@ -96,12 +148,23 @@ public sealed record SpectrAcqDevice(
         return (string)result.Results["serialNumber"];
     }
 
+    /// <summary>
+    /// Sets the integration time of the SAQ by sending the saq3_setIntegrationTime command 
+    /// </summary>
+    /// <param name="integtrationTimeInSec"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task SetIntegrationTimeAsync(int integtrationTimeInSec, CancellationToken cancellationToken = default)
     {
         return Communicator.SendAsync(new SaqSetIntegrationTimeCommand(DeviceId, integtrationTimeInSec),
             cancellationToken);
     }
 
+    /// <summary>
+    /// Retrieves the integration time of the SAQ by sending the saq3_getIntegrationTime command 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<int> GetIntegrationTimeAsync(CancellationToken cancellationToken = default)
     {
         var result =
@@ -109,11 +172,22 @@ public sealed record SpectrAcqDevice(
         return int.Parse(result.Results["integrationTime"].ToString());
     }
 
+    /// <summary>
+    /// Sets the bias voltage of the SAQ by sending the saq3_setHVBiasVoltage command 
+    /// </summary>
+    /// <param name="biasVoltage"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task SetHvBiasVoltageAsync(int biasVoltage, CancellationToken cancellationToken = default)
     {
         return Communicator.SendAsync(new SaqSetHvBiasVoltageCommand(DeviceId, biasVoltage), cancellationToken);
     }
 
+    /// <summary>
+    /// Retrieves the bias voltage of the SAQ by sending the saq3_getHVBiasVoltage command 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<int> GetHvBiasVoltageAsync(CancellationToken cancellationToken = default)
     {
         var result =
@@ -121,13 +195,27 @@ public sealed record SpectrAcqDevice(
         return int.Parse(result.Results["biasVoltage"].ToString());
     }
 
+    /// <summary>
+    /// Retrieves the maximum allowed HV voltage of the SAQ by sending the saq3_getMaxHVVoltageAllowed command 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<int> GetMaxHvVoltageAllowedAsync(CancellationToken cancellationToken = default)
     {
         var result =
             await Communicator.SendWithResponseAsync(new SaqGetMaxHvVoltageAllowedCommand(DeviceId), cancellationToken);
-        return int.Parse(result.Results["biasVoltage"].ToString()) ;
+        return int.Parse(result.Results["biasVoltage"].ToString());
     }
 
+    /// <summary>
+    /// Defines the acquisition set of the SAQ by sending the saq3_defineAcqSet command 
+    /// </summary>
+    /// <param name="scanCount"></param>
+    /// <param name="timeStep"></param>
+    /// <param name="integrationTime"></param>
+    /// <param name="externalParam"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task DefineAcqSetAsync(int scanCount, int timeStep, int integrationTime, int externalParam,
         CancellationToken cancellationToken = default)
     {
@@ -136,32 +224,63 @@ public sealed record SpectrAcqDevice(
             cancellationToken);
     }
 
+    /// <summary>
+    /// Retrieves the defined acquisition set of the SAQ by sending the saq3_getAcqSet command 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<Dictionary<string, object>> GetAcqSetAsync(CancellationToken cancellationToken = default)
     {
         var result = await Communicator.SendWithResponseAsync(new SaqGetAcqSetCommand(DeviceId), cancellationToken);
         return result.Results;
     }
 
+    /// <summary>
+    /// Starts the data acquisition of the SAQ by sending the saq3_acqStart command
+    /// </summary>
+    /// <param name="triggerMode"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task StartAcquisitionAsync(TriggerMode triggerMode, CancellationToken cancellationToken = default)
     {
         return Communicator.SendAsync(new SaqAcqStartCommand(DeviceId, triggerMode), cancellationToken);
     }
 
+    /// <summary>
+    /// Stops the data acquisition of the SAQ by sending the saq3_acqStop command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task StopAcquisitionAsync(CancellationToken cancellationToken = default)
     {
         return Communicator.SendAsync(new SaqAcqStopCommand(DeviceId), cancellationToken);
     }
 
+    /// <summary>
+    /// Pauses the data acquisition of the SAQ by sending the saq3_acqPause command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task PauseAcquisitionAsync(CancellationToken cancellationToken = default)
     {
         return Communicator.SendAsync(new SaqAcqPauseCommand(DeviceId), cancellationToken);
     }
 
+    /// <summary>
+    /// Continues the paused data acquisition of the SAQ by sending the saq3_acqContinue command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task ContinueAcquisitionAsync(CancellationToken cancellationToken = default)
     {
         return Communicator.SendAsync(new SaqAcqContinueCommand(DeviceId), cancellationToken);
     }
 
+    /// <summary>
+    /// Checks if there is any acquisition data of the SAQ available to retrieve by sending the saq3_isDataAvailable command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<bool> GetIsDataAvailableAsync(CancellationToken cancellationToken = default)
     {
         var result =
@@ -169,6 +288,11 @@ public sealed record SpectrAcqDevice(
         return (bool)result.Results["isDataAvailable"];
     }
 
+    /// <summary>
+    /// Retrieves available acquisition data of the SAQ by sending the saq3_getAvailableData command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<SaqData> GetAvailableDataAsync(
         CancellationToken cancellationToken = default)
     {
@@ -179,16 +303,33 @@ public sealed record SpectrAcqDevice(
         return data;
     }
 
+    /// <summary>
+    /// Forces the trigger of the SAQ by sending the saq3_forceTrigger command
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task ForceTriggerAsync(CancellationToken cancellationToken = default)
     {
         return Communicator.SendAsync(new SaqForceTriggerCommand(DeviceId), cancellationToken);
     }
 
+    /// <summary>
+    /// Sets the input trigger mode of the SAQ by sending the saq3_setInTriggerMode command
+    /// </summary>
+    /// <param name="inTriggerMode"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task SetInTriggerModeAsync(InTriggerMode inTriggerMode, CancellationToken cancellationToken = default)
     {
         return Communicator.SendAsync(new SaqSetInTriggerModeCommand(DeviceId, inTriggerMode), cancellationToken);
     }
 
+    /// <summary>
+    /// Retrieves the different trigger modes that are set of the SAQ by sending the saq3_getTriggerMode command.
+    /// The following modes are returned: "scanStartMode", "inputTriggerMode", "outputTriggerMode", "ccdMode"
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<Dictionary<string, object>> GetTriggerModeAsync(
         CancellationToken cancellationToken = default)
     {
@@ -197,6 +338,11 @@ public sealed record SpectrAcqDevice(
         return (Dictionary<string, object>)result.Results["results"];
     }
 
+    /// <summary>
+    /// Retrieves the last error of the SAQ by sending the saq3_getLastError command.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<string> GetLastErrorAsync(
         CancellationToken cancellationToken = default)
     {
@@ -205,6 +351,11 @@ public sealed record SpectrAcqDevice(
         return (string)result.Results["error"];
     }
 
+    /// <summary>
+    /// Retrieves the error log of the SAQ by sending the saq3_getErrorLog command.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<string> GetErrorLogAsync(
         CancellationToken cancellationToken = default)
     {
@@ -212,7 +363,12 @@ public sealed record SpectrAcqDevice(
             await Communicator.SendWithResponseAsync(new SaqGetErrorLogCommand(DeviceId), cancellationToken);
         return (string)result.Results["errors"];
     }
-    
+
+    /// <summary>
+    /// Cleans the error log of the SAQ by sending the saq3_clearErrorLog command.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public Task ClearErrorLogAsync(CancellationToken cancellationToken = default)
     {
         return Communicator.SendAsync(new SaqClearErrorLogCommand(DeviceId), cancellationToken);
