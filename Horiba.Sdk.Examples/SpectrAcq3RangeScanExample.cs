7a06b0ceb4059e9c8ac841d0da4ef7f8c21319f3
DESKTOP-R7JVAOO\Dell_Z84534 <nikolaus.naredi-rainer@zuehlke.com>
2025-04-03 14:41:32 +0200

    added example for range scan with spectracq3

---
+++ Horiba.Sdk.Examples/SpectrAcq3RangeScanExample.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Horiba.Sdk.Devices;
+using Horiba.Sdk.Enums;
+using Serilog;
+using ScottPlot;
+
+namespace Horiba.Sdk.Examples
+{
+    public class SpectrAcq3RangeScanExample
+    {
+        public static async Task MainAsync()
+        {
+            var deviceManager = new DeviceManager();
+            await deviceManager.StartAsync();
+
+            if (!deviceManager.SpectrAcqDevices.Any() || !deviceManager.Monochromators.Any())
+            {
+                Log.Error("Required monochromator or SpectrAcq3 not found");
+                await deviceManager.StopAsync();
+                return;
+            }
+
+            var mono = deviceManager.Monochromators.First();
+            await mono.OpenConnectionAsync();
+            await WaitForMonoAsync(mono);
+
+            var spectracq3 = deviceManager.SpectrAcqDevices.First();
+            await spectracq3.OpenConnectionAsync();
+
+            var startWavelength = 490;
+            var endWavelength = 520;
+            var incrementWavelength = 3;
+            var wavelengths = Enumerable.Range(startWavelength, (endWavelength - startWavelength) / incrementWavelength + 1)
+                                        .Select(i => startWavelength + i * incrementWavelength).ToList();
+            var xData = new List<int>();
+            var yDataCurrent = new List<float>();
+            var yDataVoltage = new List<float>();
+            var yDataCounts = new List<float>();
+
+            try
+            {
+                await spectracq3.SetAcqSetAsync(1, 0, 1, 0);
+
+                foreach (var wavelength in wavelengths)
+                {
+                    await mono.MoveToWavelengthAsync(wavelength);
+                    while (await mono.IsDeviceBusyAsync())
+                    {
+                        await Task.Delay(100);
+                    }
+                    Log.Information($"Monochromator set to {wavelength}nm");
+
+                    await spectracq3.StartAcquisitionAsync(ScanStartMode.TriggerAndInterval);
+                    await Task.Delay(3000);
+                    var data = await spectracq3.GetAvailableDataAsync();
+                    Log.Information($"Acquired data at {wavelength}nm: {data}");
+                    xData.Add(wavelength);
+                    yDataCurrent.Add(data.Data[0].CurrentSignal.Value);
+                    yDataVoltage.Add(data.Data[0].VoltageSignal.Value);
+                    yDataCounts.Add(data.Data[0].PpdSignal.Value);
+                }
+            }
+            finally
+            {
+                await mono.CloseConnectionAsync();
+                await spectracq3.CloseConnectionAsync();
+                await deviceManager.StopAsync();
+            }
+
+            await PlotValuesAsync(startWavelength, endWavelength, xData, yDataCurrent);
+        }
+
+        private static async Task PlotValuesAsync(int startWavelength, int endWavelength, List<int> xData, List<float> yData)
+        {
+            var plt = new ScottPlot.Plot();
+            plt.Add.Scatter(xData.Select(x => (double)x).ToArray(), yData.Select(y => (double)y).ToArray());
+            plt.Title($"Range Scan {startWavelength}-{endWavelength}[nm] vs. Intensity");
+            plt.XLabel("Wavelength");
+            plt.YLabel("Intensity");
+            plt.SavePng("SpectrAcq3RangeScanPlot.png", 400, 300);
+        }
+
+        private static async Task WaitForMonoAsync(MonochromatorDevice mono)
+        {
+            bool monoBusy = true;
+            while (monoBusy)
+            {
+                monoBusy = await mono.IsDeviceBusyAsync();
+                await Task.Delay(1000);
+                Log.Information("Mono busy...");
+            }
+        }
+    }
+}
