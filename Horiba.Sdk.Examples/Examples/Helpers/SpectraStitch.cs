e52343db6590c6563c4877e72e8b4792f3b57123
peter-bartalis <peter.bartalis@horiba.com>
2025-10-03 10:35:43 -0400

    Delete Horiba.Sdk.Examples/Examples/Helpers/SpectraStitch.cs

--- Horiba.Sdk.Examples/Examples/Helpers/SpectraStitch.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Horiba.Sdk.Examples;
-public abstract class SpectraStitch
-{
-    public abstract SpectraStitch StitchWith(SpectraStitch otherStitch);
-    public abstract List<List<float>> StitchedSpectra();
-}
